2ea083804a3b92df73103b70bcfa87f634f116ba
vanshikapandey <pvanshik217@gmail.com>
2020-07-07 23:52:44 +0530

    bugs to be removed

--- client/src/components/navcomponents/team.js
+++ client/src/components/navcomponents/team.js
@@ -3,13 +3,15 @@ import React from "react";
 const Team = () => {
   return (
     <div className='team'>
-      <h1 style={{ color: "white" }}>Start from here!</h1>
+      <h1 data-aos="zoom-right" data-aos-duration = "2000" className ="coreteam"style={{ color: "white"  }}>Core Team</h1>
 
-    <div className ='begin'>
-        <div className="box">
+    <div className ="container">
+          <div class="row">
+            <div class='col-md-6 col-lg-6'>
+            <div data-aos="zoom-out-right" data-aos-duration = "2000"className="box" >
             <div className="bg">
               <div className='dp'>
-                 <img src="dp.png" alt=""/>
+                 <img src="client/src/components/navcomponents/logo.png" alt=""/>
               </div>
             </div>
             <div class="name" >
@@ -20,7 +22,7 @@ const Team = () => {
             <div className="skills">
                 <ul>
                     <li className="html">HTML 70%</li>
-                    <li clclassNameass="css">CSS  80%</li>
+                    <li className="css">CSS  80%</li>
                     <li className="javascript">JAVASCRIPT 90%</li>
                     <li className="react">REACT 60%</li>
                 </ul>
@@ -44,12 +46,226 @@ const Team = () => {
                 </div>
             </div>
         </div>
-
-
-    </div>
+      </div>
+    
+    
     
 
+      <div class='col-md-6 col-lg-6'>
+        <div data-aos="zoom-out-right" data-aos-duration = "2000"className="box">
+            <div className="bg">
+              <div className='dp'>
+                 <img src="dp.png" alt=""/>
+              </div>
+            </div>
+            <div class="name" >
+                <h2 data-aos="fade-left" data-aos-duration ="1000">Vanshika Pandey</h2>
+                <h5 data-aos="fade-left" data-aos-duration ="3000"><hr class="mlline"></hr>ML Enthusiast<hr class="mlline"></hr></h5>
+            </div>
+           
+            <div className="skills">
+                <ul>
+                    <li className="html">HTML 70%</li>
+                    <li className="css">CSS  80%</li>
+                    <li className="javascript">JAVASCRIPT 90%</li>
+                    <li className="react">REACT 60%</li>
+                </ul>
+            </div>
+            <div className="move">
+            <div className="bluebox">
+                    <div className="icons">
+                        <div class="i" >
+                            <a href="#" class="fa fa-linkedin-square"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-instagram"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-facebook-official"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-github"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    
+    
+
+    
+        
+      <div class='col-md-6 col-lg-6'>
+        <div data-aos="zoom-out-right" data-aos-duration = "2000"className="box">
+            <div className="bg">
+              <div className='dp'>
+                 <img src="dp.png" alt=""/>
+              </div>
+            </div>
+            <div class="name" >
+                <h2 data-aos="fade-left" data-aos-duration ="1000">Vanshika Pandey</h2>
+                <h5 data-aos="fade-left" data-aos-duration ="3000"><hr class="mlline"></hr>ML Enthusiast<hr class="mlline"></hr></h5>
+            </div>
+           
+            <div className="skills">
+                <ul>
+                    <li className="html">HTML 70%</li>
+                    <li className="css">CSS  80%</li>
+                    <li className="javascript">JAVASCRIPT 90%</li>
+                    <li className="react">REACT 60%</li>
+                </ul>
+            </div>
+            <div className="move">
+            <div className="bluebox">
+                    <div className="icons">
+                        <div class="i" >
+                            <a href="#" class="fa fa-linkedin-square"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-instagram"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-facebook-official"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-github"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    
+        
+      <div class='col-md-6 col-lg-6'>
+        <div data-aos="zoom-out-right" data-aos-duration = "2000"className="box">
+            <div className="bg">
+              <div className='dp'>
+                 <img src="dp.png" alt=""/>
+              </div>
+            </div>
+            <div class="name" >
+                <h2 data-aos="fade-left" data-aos-duration ="1000">Vanshika Pandey</h2>
+                <h5 data-aos="fade-left" data-aos-duration ="3000"><hr class="mlline"></hr>ML Enthusiast<hr class="mlline"></hr></h5>
+            </div>
+           
+            <div className="skills">
+                <ul>
+                    <li className="html">HTML 70%</li>
+                    <li className="css">CSS  80%</li>
+                    <li className="javascript">JAVASCRIPT 90%</li>
+                    <li className="react">REACT 60%</li>
+                </ul>
+            </div>
+            <div className="move">
+            <div className="bluebox">
+                    <div className="icons">
+                        <div class="i" >
+                            <a href="#" class="fa fa-linkedin-square"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-instagram"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-facebook-official"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-github"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    
+
+      <div class='col-md-6 col-lg-6'>
+        <div data-aos="zoom-out-right" data-aos-duration = "2000"className="box">
+            <div className="bg">
+              <div className='dp'>
+                 <img src="dp.png" alt=""/>
+              </div>
+            </div>
+            <div class="name" >
+                <h2 data-aos="fade-left" data-aos-duration ="1000">Vanshika Pandey</h2>
+                <h5 data-aos="fade-left" data-aos-duration ="3000"><hr class="mlline"></hr>ML Enthusiast<hr class="mlline"></hr></h5>
+            </div>
+           
+            <div className="skills">
+                <ul>
+                    <li className="html">HTML 70%</li>
+                    <li className="css">CSS  80%</li>
+                    <li className="javascript">JAVASCRIPT 90%</li>
+                    <li className="react">REACT 60%</li>
+                </ul>
+            </div>
+            <div className="move">
+            <div className="bluebox">
+                    <div className="icons">
+                        <div class="i" >
+                            <a href="#" class="fa fa-linkedin-square"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-instagram"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-facebook-official"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-github"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    
+
+      <div class='col-md-6 col-lg-6'>
+        <div data-aos="zoom-out-right" data-aos-duration = "2000"className="box">
+            <div className="bg">
+              <div className='dp'>
+                 <img src="dp.png" alt=""/>
+              </div>
+            </div>
+            <div class="name" >
+                <h2 data-aos="fade-left" data-aos-duration ="1000">Vanshika Pandey</h2>
+                <h5 data-aos="fade-left" data-aos-duration ="3000"><hr class="mlline"></hr>ML Enthusiast<hr class="mlline"></hr></h5>
+            </div>
+           
+            <div className="skills">
+                <ul>
+                    <li className="html">HTML 70%</li>
+                    <li className="css">CSS  80%</li>
+                    <li className="javascript">JAVASCRIPT 90%</li>
+                    <li className="react">REACT 60%</li>
+                </ul>
+            </div>
+            <div className="move">
+            <div className="bluebox">
+                    <div className="icons">
+                        <div class="i" >
+                            <a href="#" class="fa fa-linkedin-square"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-instagram"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-facebook-official"></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-github"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+      </div>
     </div>
+  </div>
+
   );
 };
 
