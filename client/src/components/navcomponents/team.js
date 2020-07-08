504103ab33bacec77fc65258e815ae80cddb1786
Vanshika Pandey <56474909+vanshikapandey@users.noreply.github.com>
2020-07-08 23:29:55 +0530

    modified team , page resizing & logo left

--- client/src/components/navcomponents/team.js
+++ client/src/components/navcomponents/team.js
@@ -1,13 +1,38 @@
 import React from "react";
+// import logo from "../img/logo.png";
+//   import one from "../img/1.jpg";
 
 const Team = () => {
   return (
     <div className='team'>
-      <h1 data-aos="zoom-right" data-aos-duration = "2000" className ="coreteam"style={{ color: "white"  }}>Our Team</h1>
+      <div class="teamhead">
+      <div className='landinghead'>
+            <p>
+              <img
+                data-aos='fade-up-right'
+                data-aos-duration='1800'
+                className='img1'
+                src='client/src/components/navcomponents/logo.png'
+                alt='#'
+              ></img>
+              <span data-aos='zoom-in-right' className='long'>
+               Our Team</span>
+              {/* <span className='short'>CodeCops</span> */}
+              <img
+                data-aos='fade-up-left'
+                data-aos-duration='1800'
+                src='https://upload.wikimedia.org/wikipedia/en/c/cf/Iiit-una-logo.png'
+                alt='#'
+                className='img2'
+              ></img>
+            </p>
+          </div>
+        </div>
+      {/* <h1 data-aos="zoom-right" data-aos-duration = "2000" className ="coreteam"style={{ color: "white"  }}>Our Team</h1> */}
 
     <div className ="container">
           <div class="row">
-            <div class='col-md-4 col-lg-4' class="space">
+            <div class='col-md-4 col-lg-4' class="space" className="param" className="p" >
             <div data-aos="zoom-out-right" data-aos-duration = "2000"className="box" >
             <div className="bg">
               <div className='dp'>
@@ -15,32 +40,35 @@ const Team = () => {
               </div>
             </div>
             <div class="name" >
-                <h2 data-aos="fade-left" data-aos-duration ="1000">Vanshika Pandey</h2>
-                <h5 data-aos="fade-left" data-aos-duration ="3000">ML Enthusiast</h5>
+                <h2 data-aos="fade-left" data-aos-duration ="1000">Paramjeet Singh</h2>
+                <h5 data-aos="fade-left" data-aos-duration ="3000">Android Developer</h5>
             </div>
             <hr class="mlline"></hr>
             <div className="skills">
                 <ul>
-                  <li className='html'>HTML 70%</li>
-                  <li className='css'>CSS 80%</li>
-                  <li className='javascript'>JAVASCRIPT 90%</li>
-                  <li className='react'>REACT 60%</li>
+                  <li className='s1'>Android Development 90%</li>
+                  <li className='s2'>Java 80%</li>
+                  <li className='s3'>Python 70%</li>
+                  <li className='s5'>Firebase 70%</li>
+                  <li className='s8'>Javascript 60%</li>
+                  <li className='s9'>MySQL 60%</li> 
+
                 </ul>
             </div>
-            <div className="move">
+            <div className="move">                
             <div className="bluebox">
                     <div className="icons">
                         <div class="i" >
-                            <a href="#" ><i class="fa fa-linkedin-square"></i></a>
+                            <a href="https://www.linkedin.com/in/iampjeetsingh" ><i class="fa fa-linkedin-square"></i></a>
                         </div>
                         <div className="i" >
-                        <a href="#" ><i class="fa fa-instagram"></i></a>
+                        <a href="https://instagram.com/param.git" ><i class="fa fa-instagram"></i></a>
                         </div>
                         <div className="i" >
-                        <a href="#" ><i class="fa fa-facebook-official"></i></a>
+                        <a href="https://facebook.com/iampjeetsingh" ><i class="fa fa-facebook-official"></i></a>
                         </div>
                         <div className="i" >
-                            <a href="#" class="fa fa-github"></a>
+                            <a href="https://github.com/iampjeetsingh" class="fa fa-github"></a>
                         </div>
                     </div>
                   </div>
@@ -49,7 +77,7 @@ const Team = () => {
             </div>
           
 
-          <div class='col-md-4 col-lg-4' class="space">
+          <div class='col-md-4 col-lg-4' class="space" className="kohli" className="p">
             <div data-aos="zoom-out-right" data-aos-duration = "2000"className="box" >
             <div className="bg">
               <div className='dp'>
@@ -57,16 +85,19 @@ const Team = () => {
               </div>
             </div>
             <div class="name" >
-                <h2 data-aos="fade-left" data-aos-duration ="1000">Vanshika Pandey</h2>
-                <h5 data-aos="fade-left" data-aos-duration ="3000">ML Enthusiast</h5>
+                <h2 data-aos="fade-left" data-aos-duration ="1000">Harsh Kohli</h2>
+                <h5 data-aos="fade-left" data-aos-duration ="3000">Web Developer</h5>
             </div>
             <hr class="mlline"></hr>
             <div className="skills">
                 <ul>
-                  <li className='html'>HTML 70%</li>
-                  <li className='css'>CSS 80%</li>
-                  <li className='javascript'>JAVASCRIPT 90%</li>
-                  <li className='react'>REACT 60%</li>
+                  <li className='s1'>HTML 90%</li>
+                  <li className='s2'>CSS 60%</li>
+                  <li className='s3'>NodeJs 70%</li>
+                  <li className='s4'>React 80%</li>
+                  <li className='s5'>C 80%</li>
+                  <li className='s6 '>JavaScript 85%</li>
+                  
                 </ul>
             </div>
             <div className="move">
@@ -92,7 +123,7 @@ const Team = () => {
           
 
         
-          <div class='col-md-4 col-lg-4' class="space">
+          <div class='col-md-4 col-lg-4' class="space" className="sarvesh" className="p">
             <div data-aos="zoom-out-right" data-aos-duration = "2000"className="box" >
             <div className="bg">
               <div className='dp'>
@@ -100,23 +131,23 @@ const Team = () => {
               </div>
             </div>
             <div class="name" >
-                <h2 data-aos="fade-left" data-aos-duration ="1000">Vanshika Pandey</h2>
+                <h2 data-aos="fade-left" data-aos-duration ="1000">Sarvesh Srivastava</h2>
                 <h5 data-aos="fade-left" data-aos-duration ="3000">ML Enthusiast</h5>
             </div>
             <hr class="mlline"></hr>
             <div className="skills">
                 <ul>
-                  <li className='html'>HTML 70%</li>
-                  <li className='css'>CSS 80%</li>
-                  <li className='javascript'>JAVASCRIPT 90%</li>
-                  <li className='react'>REACT 60%</li>
+                  <li className='s1'>C 95%</li>
+                  <li className='s2'>C++ 85%</li>
+                  <li className='s3'>Python 75%</li>
+                  <li className='s4'>ML 37%</li>
                 </ul>
             </div>
             <div className="move">
             <div className="bluebox">
                     <div className="icons">
                         <div class="i" >
-                            <a href="#" ><i class="fa fa-linkedin-square"></i></a>
+                            <a href="https://www.linkedin.com/in/sarvesh-singh-310a4718b" ><i class="fa fa-linkedin-square"></i></a>
                         </div>
                         <div className="i" >
                         <a href="#" ><i class="fa fa-instagram"></i></a>
@@ -125,7 +156,7 @@ const Team = () => {
                         <a href="#" ><i class="fa fa-facebook-official"></i></a>
                         </div>
                         <div className="i" >
-                            <a href="#" class="fa fa-github"></a>
+                            <a href="https://github.com/Sarvesh326" class="fa fa-github"></a>
                         </div>
                     </div>
                   </div>
@@ -134,7 +165,7 @@ const Team = () => {
             </div>
           
    
-            <div class='col-md-4 col-lg-4' class="space">
+            <div class='col-md-4 col-lg-4' class="space" className="deswal" className="p">
             <div data-aos="zoom-out-right" data-aos-duration = "2000"className="box" >
             <div className="bg">
               <div className='dp'>
@@ -142,32 +173,35 @@ const Team = () => {
               </div>
             </div>
             <div class="name" >
-                <h2 data-aos="fade-left" data-aos-duration ="1000">Vanshika Pandey</h2>
+                <h2 data-aos="fade-left" data-aos-duration ="1000">Abhimanyu Deswal</h2>
                 <h5 data-aos="fade-left" data-aos-duration ="3000">ML Enthusiast</h5>
             </div>
             <hr class="mlline"></hr>
             <div className="skills">
                 <ul>
-                  <li className='html'>HTML 70%</li>
-                  <li className='css'>CSS 80%</li>
-                  <li className='javascript'>JAVASCRIPT 90%</li>
-                  <li className='react'>REACT 60%</li>
+                  <li className='s1'>C++  70%</li>
+                  <li className='s2'>C  80%</li>
+                  <li className='s3'>Python  90%</li>
+                  <li className='s4'>Android Studio  60%</li>
+                  <li className='s5'>HTML  90%</li>
+                  <li className='s6'>CSS  90%</li>
+                
                 </ul>
             </div>
             <div className="move">
             <div className="bluebox">
                     <div className="icons">
                         <div class="i" >
-                            <a href="#" ><i class="fa fa-linkedin-square"></i></a>
+                            <a href="https://instagram.com/_.abhiimanyu." ><i class="fa fa-linkedin-square"></i></a>
                         </div>
                         <div className="i" >
-                        <a href="#" ><i class="fa fa-instagram"></i></a>
+                        <a href="https://www.linkedin.com/in/abhimanyu-deswal" ><i class="fa fa-instagram"></i></a>
                         </div>
                         <div className="i" >
-                        <a href="#" ><i class="fa fa-facebook-official"></i></a>
+                        <a href="https://facebook.com/AbhimanyuDeswal" ><i class="fa fa-facebook-official"></i></a>
                         </div>
                         <div className="i" >
-                            <a href="#" class="fa fa-github"></a>
+                            <a href="https://github.com/AbhimanyuDeswal" class="fa fa-github"></a>
                         </div>
                     </div>
                   </div>
@@ -176,7 +210,7 @@ const Team = () => {
             </div>
           
 
-            <div class='col-md-4 col-lg-4' class="space">
+            <div class='col-md-4 col-lg-4' class="space" className="sarthak" className="p">
             <div data-aos="zoom-out-right" data-aos-duration = "2000"className="box" >
             <div className="bg">
               <div className='dp'>
@@ -184,23 +218,23 @@ const Team = () => {
               </div>
             </div>
             <div class="name" >
-                <h2 data-aos="fade-left" data-aos-duration ="1000">Vanshika Pandey</h2>
+                <h2 data-aos="fade-left" data-aos-duration ="1000">Sarthak Mehta</h2>
                 <h5 data-aos="fade-left" data-aos-duration ="3000">ML Enthusiast</h5>
             </div>
             <hr class="mlline"></hr>
             <div className="skills">
                 <ul>
-                  <li className='html'>HTML 70%</li>
-                  <li className='css'>CSS 80%</li>
-                  <li className='javascript'>JAVASCRIPT 90%</li>
-                  <li className='react'>REACT 60%</li>
+                  <li className='s1'>C++ 70%</li>
+                  <li className='s2'>Open gl 50%</li>
+                  <li className='s3'>Blender 50%</li>
+                  <li className='s4'>C 80%</li>
                 </ul>
             </div>
             <div className="move">
             <div className="bluebox">
                     <div className="icons">
                         <div class="i" >
-                            <a href="#" ><i class="fa fa-linkedin-square"></i></a>
+                            <a href="https://www.linkedin.com/in/sarthak-mehta-a42183191" ><i class="fa fa-linkedin-square"></i></a>
                         </div>
                         <div className="i" >
                         <a href="#" ><i class="fa fa-instagram"></i></a>
@@ -209,7 +243,7 @@ const Team = () => {
                         <a href="#" ><i class="fa fa-facebook-official"></i></a>
                         </div>
                         <div className="i" >
-                            <a href="#" class="fa fa-github"></a>
+                            <a href="https://github.com/sarthakmehta9999" class="fa fa-github"></a>
                         </div>
                     </div>
                   </div>
@@ -217,7 +251,7 @@ const Team = () => {
               </div>
             </div>
           
-            <div class='col-md-4 col-lg-4' class="space">
+            <div class='col-md-4 col-lg-4' class="space" className="avi" className="p">
             <div data-aos="zoom-out-right" data-aos-duration = "2000"className="box" >
             <div className="bg">
               <div className='dp'>
@@ -225,7 +259,7 @@ const Team = () => {
               </div>
             </div>
             <div class="name" >
-                <h2 data-aos="fade-left" data-aos-duration ="1000">Vanshika Pandey</h2>
+                <h2 data-aos="fade-left" data-aos-duration ="1000">Avi Singh</h2>
                 <h5 data-aos="fade-left" data-aos-duration ="3000">ML Enthusiast</h5>
             </div>
             <hr class="mlline"></hr>
@@ -235,6 +269,7 @@ const Team = () => {
                   <li className='css'>CSS 80%</li>
                   <li className='javascript'>JAVASCRIPT 90%</li>
                   <li className='react'>REACT 60%</li>
+                
                 </ul>
             </div>
             <div className="move">
@@ -257,6 +292,50 @@ const Team = () => {
                 </div>
               </div>
             </div>  
+
+
+            <div class='col-md-4 col-lg-4' class="space" className="piyush" className="p">
+            <div data-aos="zoom-out-right" data-aos-duration = "2000"className="box" >
+            <div className="bg">
+              <div className='dp'>
+                 <img src="client/src/components/navcomponents/logo.png" alt=""/>
+              </div>
+            </div>
+            <div class="name" >
+                <h2 data-aos="fade-left" data-aos-duration ="1000">Piyush Aggarwal</h2>
+                <h5 data-aos="fade-left" data-aos-duration ="3000">ML Enthusiast</h5>
+            </div>
+            <hr class="mlline"></hr>
+            <div className="skills">
+                <ul>
+                  <li className='html'>HTML 70%</li>
+                  <li className='css'>CSS 80%</li>
+                  <li className='javascript'>JAVASCRIPT 90%</li>
+                  <li className='react'>REACT 60%</li>
+                  
+                </ul>
+            </div>
+            <div className="move">
+            <div className="bluebox">
+                    <div className="icons">
+                        <div class="i" >
+                            <a href="#" ><i class="fa fa-linkedin-square"></i></a>
+                        </div>
+                        <div className="i" >
+                        <a href="#" ><i class="fa fa-instagram"></i></a>
+                        </div>
+                        <div className="i" >
+                        <a href="#" ><i class="fa fa-facebook-official"></i></a>
+                        </div>
+                        <div className="i" >
+                            <a href="#" class="fa fa-github"></a>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>  
+
          </div>
     </div>
 </div>
